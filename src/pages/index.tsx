73277f121eb95a67475bd5541c1cd2892bb0d731
keith-loww <keithlow1@hotmail.com>
2024-01-13 12:22:13 -0800

    Update custom.css and index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,17 +10,20 @@ import styles from "./index.module.css";
 function HomepageHeader() {
     const { siteConfig } = useDocusaurusContext();
     return (
-        <header className={clsx("hero hero--primary", styles.heroBanner)}>
-            <div className="container">
-                <Heading as="h1" className="hero__title font-poppins">
+        <header className="flex flex-wrap place-items-center justify-center bg-blue-950 px-6 py-32">
+            <div className="flex flex-wrap place-content-center place-items-center gap-y-2">
+                <Heading
+                    as="h1"
+                    className="font-poppins w-full text-center text-7xl"
+                >
                     {siteConfig.title}
                 </Heading>
-                <p className="hero__subtitle font-poppins">
+                <p className="font-poppins w-full text-center text-xl">
                     {siteConfig.tagline}
                 </p>
                 <div className={styles.buttons}>
                     <Link
-                        className="button button--secondary button--lg"
+                        className="button button--secondary button--lg font-poppins"
                         to="/docs/guide"
                     >
                         Get Started 🚀
