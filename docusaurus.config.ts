00beae85e9f45da7c510e1243db40d09054e690e
keith-loww <keithlow1@hotmail.com>
2024-02-05 13:10:44 -0800

    Update Docusaurus configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,16 @@ const config: Config = {
     favicon: "img/favicon.ico",
 
     // Set the production url of your site here
-    url: "https://your-docusaurus-site.example.com",
+    url: "https://codelab-davis.github.io/codelab-toolkit/",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: "/",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: "CodeLabDavis", // Usually your GitHub org/user name.
+    organizationName: "Codelab-davis", // Usually your GitHub org/user name.
     projectName: "codelab-toolkit", // Usually your repo name.
+    deploymentBranch: "gh-pages",
 
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "warn",
