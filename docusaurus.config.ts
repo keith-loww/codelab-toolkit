66b431a4c7598ae4370b5d8e8aeacc72448264ba
keith-loww <keithlow1@hotmail.com>
2024-11-16 18:28:05 -0800

    Update Docusaurus configuration for GitHub Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,14 +8,14 @@ const config: Config = {
     favicon: "img/favicon.ico",
 
     // Set the production url of your site here
-    url: "http://toolkit.codelabdavis.com/",
+    url: "https://keith-loww.github.io",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/",
+    baseUrl: "/codelab-toolkit",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: "Codelab-davis", // Usually your GitHub org/user name.
+    organizationName: "keith-loww", // Usually your GitHub org/user name.
     projectName: "codelab-toolkit", // Usually your repo name.
     deploymentBranch: "gh-pages",
 
